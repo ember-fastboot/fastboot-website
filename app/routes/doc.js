3a4fbee896536bf36223896491fa1adc770ca8c7
David Pett <dpett@folklore.digital>
2016-03-29 07:09:16 -0500

    sync markdown

--- app/routes/doc.js
+++ app/routes/doc.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
+import markdownFiles from 'ember-fr-markdown-file/markdownFiles';
+
+const { get } = Ember;
 
 export default Ember.Route.extend({
   model(params) {
-    return params.path;
+    return get(markdownFiles, params.path.replace(/\//g, '.'));
   }
 });
