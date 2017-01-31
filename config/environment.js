25e192be5b38f689c527ac7c932a37e6464e120f
simon <ihmig@kaliber5.de>
2017-01-31 20:46:09 +0100

    Upgraded to ember-cli 2.11, latest ember-cli-fastboot & ember-cli-head (that fixed EventDispatcher exception in FastBoot)

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'fastboot-website',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     metricsAdapters: [
       {
@@ -38,7 +38,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
