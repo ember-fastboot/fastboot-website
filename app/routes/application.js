589a0362aca4c7deafc27f3a8ee03953d4f3427d
Josh Hollinshead <joshuamarkhollinshead@gmail.com>
2017-02-08 21:04:46 +1300

    Use ember-href-to instead of handling inter-page clicks manually

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,29 +1,8 @@
 import Ember from 'ember';
 import markdownFiles from 'ember-fr-markdown-file/markdownFiles';
-import injectService from 'ember-service/inject';
-
 const { get } = Ember;
 
 export default Ember.Route.extend({
-  fastboot: injectService(),
-
-  init() {
-    if (!get(this, 'fastboot.isFastBoot')) {
-      Ember.$(document).on('click', 'a', function(e) {
-        if (!e.ctrlKey && !e.metaKey && e.target.target !== '_blank') {
-          let target = e.target || e.currentTarget;
-          if (target.host === window.location.host && !target.hash) {
-            e.preventDefault();
-            if (target.pathname === '/') {
-              this.transitionTo('index');
-            } else {
-              this.transitionTo('page', target.pathname.replace(/^\//, ''));
-            }
-          }
-        }
-      }.bind(this));
-    }
-  },
   model() {
     return get(markdownFiles, 'intro');
   }
