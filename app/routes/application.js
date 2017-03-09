a53f87f69d83968806e0bd634203efd9cacd6640
Josh Hollinshead <joshuamarkhollinshead@gmail.com>
2017-03-10 08:01:51 +1300

    Use ember-href-to instead of handling inter-page clicks manually (#59)

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,39 +1,9 @@
 import Ember from 'ember';
 import markdownFiles from 'ember-fr-markdown-file/markdownFiles';
-import injectService from 'ember-service/inject';
 
-const { $, get } = Ember;
+const { get } = Ember;
 
 export default Ember.Route.extend({
-  fastboot: injectService(),
-
-  init() {
-    if (!get(this, 'fastboot.isFastBoot')) {
-      $(document).on('click', 'a', function(e) {
-        let target = e.currentTarget;
-        let $target = $(target);
-        let targetHost = target.host === '' ? window.location.host : target.host;
-
-        let isMailto = (target.href && target.href.indexOf('mailto:') > -1);
-        let shouldHandleClick = (e.which === 1 && !e.ctrlKey && !e.metaKey && target.target !== '_blank');
-
-        if (shouldHandleClick && !isMailto) {
-          if ($target.hasClass('link-to')) {
-            e.preventDefault();
-            return;
-          }
-          if (targetHost === window.location.host) {
-            let [,path] = target.href.split(target.host);
-            if (!target.hash) {
-              e.preventDefault();
-              this.transitionTo(path);
-              return;
-            }
-          }
-        }
-      }.bind(this));
-    }
-  },
   model() {
     return get(markdownFiles, 'intro');
   }
